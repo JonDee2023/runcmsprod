ac3ea87d3cfe33432b5d6749429e5deef55c6e41
JOHN DADA <123139569+JonDee2023@users.noreply.github.com>
2026-07-14 19:16:20 +0400

    Update db-connection.js
    Saved connection string for supabase

--- db-connection.js
+++ db-connection.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "runcms",
-  password: "root",
-  port: 5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
