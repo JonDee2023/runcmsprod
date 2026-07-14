0444ea82db17b134c60336329a3089b860b316ff
JOHN DADA <123139569+JonDee2023@users.noreply.github.com>
2026-07-15 00:50:38 +0400

    Update runcms-signup-full.js
    Updated to render API url

--- public/runcms-signup-full.js
+++ public/runcms-signup-full.js
@@ -74,7 +74,7 @@ async function userSignup(event) {
 
     
 
-    const response = await fetch("http://localhost:3000/api/signup", {
+    const response = await fetch("https://runcmsprod.onrender.com/api/signup", {
 
     method: "POST",
 
