a87392d46113f0801b4928f7abdad9aea4fbc0ee
JOHN DADA <123139569+JonDee2023@users.noreply.github.com>
2026-07-26 08:28:19 +0100

    Update runcms-savenewpassword-full.js

--- public/runcms-savenewpassword-full.js
+++ public/runcms-savenewpassword-full.js
@@ -31,7 +31,7 @@ async function saveNewPassword(e) {
     const hashedPassword = simpleHash(newPassword);
 
     try {
-        const response = await fetch("https://runcmsprod.onrender.com/api/savenewpassword", {
+        const response = await fetch("https://runcmsprods.onrender.com/api/savenewpassword", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
